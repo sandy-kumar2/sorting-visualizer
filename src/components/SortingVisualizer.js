de95eb220d251c7d93689aed0e8d831cdf355e85
sandy-kumar2 <imsandeep721@gmail.com>
2024-12-31 13:43:18 +0530

    update

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
 import ArrayBar from "./ArrayBar";
 import "./SortingVisualizer.css";
-import {
-  FaSort,
-  FaSortAmountDown,
-  FaSortAmountUp,
-  FaSortNumericDown,
-} from "react-icons/fa";
 
 const SortingVisualizer = () => {
   //sorting logic
